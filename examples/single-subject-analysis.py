3f25231115bfbf1ae561220ba7ce86b4f2e4dbd5
Daniel Gomez <dgomez1@mgh.harvard.edu>
2022-01-31 19:45:11 -0500

    update single subject analysis

--- examples/single-subject-analysis.py
+++ examples/single-subject-analysis.py
@@ -9,6 +9,7 @@ from nilearn import image, input_data
 import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
@@ -16,6 +17,14 @@ import nibabel as nib
 
 period = 10
 stimulus_offset = 14
+
+
+os.chdir("/Users/dangom/MGH/data")
+
+# Step 1. Define the localizer mask and constrain it to V1.
+locf = "activation.nii.gz"
+v1f = "testv1.nii.gz"
+data = "bold.nii.gz"
 
 
 def get_tr(data):
@@ -31,33 +40,42 @@ def get_offset(stimulus_offset=14, period=10):
     return offset
 
 
-os.chdir("/Users/dangom/MGH/data")
+def truncate_affine(nii):
+    return image.new_img_like(
+        nii, nii.get_fdata(), np.around(nii.affine, 5), copy_header=True
+    )
 
-# Step 1. Define the localizer mask and constrain it to V1.
-loc = "activation.nii.gz"
-v1 = "testv1.nii.gz"
-data = "bold.nii.gz"
+
+def make_mask(v1, loc, threshold):
+    mask = image.math_img("v1 * loc", v1=v1, loc=loc)
+    return image.math_img(
+        "f.astype(bool).astype(int)", f=image.binarize_img(mask, threshold=threshold)
+    )
+
 
 tr = get_tr(data)
 offset = get_offset(stimulus_offset, period)
 
+v1 = truncate_affine(image.load_img(v1f))
+loc = truncate_affine(image.load_img(locf))
 
-mask = image.binarize_img(
-    image.math_img(
-        "v1 * loc",
-        v1=image.load_img(v1),
-        loc=image.load_img(loc),
-    ),
-    10,
-)
+mask = make_mask(v1, loc, 10)
 
 masker = input_data.NiftiMasker(mask_img=mask, standardize=False)
+masker.fit(data)
+# breakpoint()
+report = masker.generate_report()
+# report.save_as_html(snakemake.output["report"])
+
 
 # Step 2 tag which localizer voxels are vessels by getting the top ~ 10%
 # of voxels within mask
 activation = masker.fit_transform(loc)
 n_vessels = int(activation.size // 10)
 ind = np.argpartition(activation.squeeze(), -n_vessels)
+
+print(f"{activation.shape=}")
+print(f"{n_vessels=}")
 
 # Step 3 - pipelines for analysis.
 pipe = Pipeline(
@@ -68,23 +86,45 @@ pipe = Pipeline(
             PeriodicGridTransformer(period=period, sampling_in=tr, start_offset=offset),
         ),
         ("scale", PSCScaler()),
-        (
-            "roisel",
-            ColumnTransformer(
-                [
-                    ("vein", FeatureAverager(), ind[-n_vessels:]),
-                    ("parenchyma", FeatureAverager(), ind[:-n_vessels]),
-                ]
-            ),
-        ),
+    ]
+)
+
+ts_psc = pipe.fit_transform(data)
+
+n_trials = pipe["interp"].n_periods_
+interp_sampling_rate = pipe["interp"].sampling_out_
+
+
+min_max = Pipeline(
+    [
+        ("tatminmax", TrialAveragingTransformer(n_trials, bootstrap=False)),
+        ("minmax", MinMaxScaler()),
+    ]
+)
+min_max.fit(ts_psc)
+amplitudes = min_max["minmax"].data_range_
+labels = [
+    "vein" if x in ind[-n_vessels:] else "parenchyma" for x in range(activation.size)
+]
+dfa = pd.DataFrame(zip(labels, amplitudes), columns=["ROI", "amplitude"])
+dfa["frequency"] = 1/period
+dfa.to_csv()
+
+col_trans = ColumnTransformer(
+    [
+        ("vein", FeatureAverager(), ind[-n_vessels:]),
+        ("parenchyma", FeatureAverager(), ind[:-n_vessels]),
     ]
 )
 
 # Step 4. get power spectrum and averaged oscillation.
-ts = pipe.fit_transform(data)
+ts = col_trans.fit_transform(ts_psc)
+print(f"{ts.shape=}")
 
-n_trials = pipe["interp"].n_periods_
-interp_sampling_rate = pipe["interp"].sampling_out_
+
+print(f"{n_trials=}")
+print(f"{interp_sampling_rate=}")
+
 fft = FFTTransformer(interp_sampling_rate)
 tat = TrialAveragingTransformer(n_trials)
 
@@ -113,7 +153,7 @@ px_df = (
     .rename({"index": "frequency"}, axis=1)
 )
 
-# px_df.to_filename()
+# px_df.to_csv(snakemake.output["px"], index=False)
 
 # Save the timeseries
 osc_df = (
@@ -129,19 +169,50 @@ osc_df = (
 )
 osc_df["ci_low"] = pd.Series(emin.T.flatten())
 osc_df["ci_high"] = pd.Series(emax.T.flatten())
-# osc_df.to_filename()
+# osc_df.to_csv(snakemake.output["osc"], index=False)
+
 
 # 5.1 Plot timeseries AND bootstrap confidence
 fig, (axt, axf) = plt.subplots(nrows=2, dpi=100, figsize=plt.figaspect(0.7))
-axt.plot(oscgrid, osc[:, 0], label="vein")
-axt.fill_between(oscgrid, emin[:, 0], emax[:, 0], alpha=0.2)
-axt.plot(oscgrid, osc[:, 1], label="parenchyma")
-axt.fill_between(oscgrid, emin[:, 1], emax[:, 1], alpha=0.2)
-axt.set(ylabel="Amplitude [% BOLD]", xlabel="Time [s]")
-axt.legend()
 
-idx = fft.closest_freq_index(0.25)
+axt.plot(targetgrid, ts[:, 0], label="vein")
+axt.plot(targetgrid, ts[:, 1], label="parenchyma")
+axt.set(ylabel="% BOLD", xlabel="Time [s]")
+axt.legend(ncol=2, frameon=False, loc="lower center")
+
+idx = fft.closest_freq_index(0.6)
 axf.plot(freqs[:idx], px[:idx, 0], label="vein")
 axf.plot(freqs[:idx], px[:idx, 1], label="parenchyma")
-axf.set(ylabel="Power Spectrum [% BOLD]", xlabel="Frequency [Hz]")
-axt.legend()
+axf.set(ylabel="Power [% BOLD]", xlabel="Frequency [Hz]")
+# axf.axvspan(1/period - 0.005,1/period + 0.005, alpha=0.1, color="red")
+idx_stim = fft.closest_freq_index(1 / period)
+axf.annotate(
+    "Stimulus",
+    (freqs[idx_stim] - 0.001, px[idx_stim, 0] + 0.02),
+    size="small",
+    xytext=(-50, -10),
+    textcoords="offset points",
+    arrowprops=dict(arrowstyle="->"),
+)
+# axf.axvline(1/period)
+
+axin = axf.inset_axes([0.55, 0.55, 0.4, 0.4])
+# axin.spines["right"].set_visible(False)
+# axin.spines["top"].set_visible(False)
+# axin.spines["left"].set_visible(False)
+# axin.spines["bottom"].set_visible(False)
+
+
+axin.plot(oscgrid, osc[:, 0], label="vein")
+# axin.fill_between(oscgrid, emin[:, 0], emax[:, 0], alpha=0.2)
+axin.plot(oscgrid, osc[:, 1], label="parenchyma")
+# axin.fill_between(oscgrid, emin[:, 1], emax[:, 1], alpha=0.2)
+axin.set(
+    xlabel=f"Period = {period:.2f}s",
+    xticklabels=[],
+    yticklabels=[],
+    xticks=[],
+    yticks=[],
+)
+
+fig.suptitle(f"Response from {activation.size} voxels in V1")
