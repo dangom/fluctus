30ca5fb94d177af1b6c446af53f394d015cfd2f6
Daniel Gomez <dgomez1@mgh.harvard.edu>
2022-06-20 12:24:17 -0400

    Fix setup info

--- setup.py
+++ setup.py
@@ -7,52 +7,48 @@ from io import open
 
 from setuptools import find_packages, setup
 
-with open('osclib/__init__.py', 'r') as f:
+with open("osclib/__init__.py", "r") as f:
     for line in f:
-        if line.startswith('__version__'):
-            version = line.strip().split('=')[1].strip(' \'"')
+        if line.startswith("__version__"):
+            version = line.strip().split("=")[1].strip(" '\"")
             break
     else:
-        version = '0.0.1'
+        version = "0.0.1"
 
-with open('README.rst', 'r', encoding='utf-8') as f:
+with open("README.rst", "r", encoding="utf-8") as f:
     readme = f.read()
 
-REQUIRES = []
+REQUIRES = ["numpy", "scipy", "matplotlib", "scikit-learn", "nilearn", "nibabel"]
 
 kwargs = {
-    'name': 'osclib',
-    'version': version,
-    'description': '',
-    'long_description': readme,
-    'author': 'Daniel Gomez',
-    'author_email': 'dgomez1@mgh.harvard.edu',
-    'maintainer': 'Daniel Gomez',
-    'maintainer_email': 'dgomez1@mgh.harvard.edu',
-    'url': 'https://github.com/_/osclib',
-    'license': 'MIT/Apache-2.0',
-    'classifiers': [
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+    "name": "osclib",
+    "version": version,
+    "description": "",
+    "long_description": readme,
+    "author": "Daniel Gomez",
+    "author_email": "d.gomez@posteo.org",
+    "maintainer": "Daniel Gomez",
+    "maintainer_email": "d.gomez@posteo.org",
+    "url": "https://github.com/dangom/osclib",
+    "license": "MIT/Apache-2.0",
+    "classifiers": [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
-    'install_requires': REQUIRES,
-    'tests_require': ['coverage', 'pytest'],
-    'packages': find_packages(exclude=('tests', 'tests.*')),
-
+    "install_requires": REQUIRES,
+    "tests_require": ["coverage", "pytest"],
+    "packages": find_packages(exclude=("tests", "tests.*")),
 }
 
-#################### BEGIN USER OVERRIDES ####################
-# Add your customizations in this section.
-
-###################### END USER OVERRIDES ####################
 
 setup(**kwargs)
